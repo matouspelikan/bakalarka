716b83489ecb1586e4a8069cfd7fe2455042e664
pelikan-laptop <pelikan.mat16@gmail.com>
2023-05-03 13:39:39 +0200

    devPost initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
+        //devPost
         Config config = null;
         try {
             config = readGDB();
